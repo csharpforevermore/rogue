428d477138a3e583449c3aa9e5475a98faee1260
Chris Randle <chris@torpedotv.com>
2021-05-20 19:05:06 +0100

    Created list of tasks

---
+++ Rogue/Rogue.ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rogue.ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there, Rogue!");
+
+
+            // TODO we will generate a random dungeon
+            // TODO create random number of rooms
+            // TODO each room is of a random location
+            // TODO each room is of a random size
+            // TODO render map
+
+
+
+            // generate player's location inside a valid location (room)
+            // generate some random
+        }
+    }
+}
